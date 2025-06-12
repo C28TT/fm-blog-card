de61d936fb99a800306ed5290e431727c905ca9a
Andrestack05 <andressuarez_05@hotmail.com>
2025-06-12 18:23:17 -0500

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,10 +3,9 @@ function mostrarFechaActual() {
   const fechaActual = new Date();
 
   const opciones = { year: "numeric", month: "short", day: "numeric" };
-  const fechaFormateada = fechaActual.toLocaleDateString("en-US", opciones); // Ej: Jun 10, 2025
+  const fechaFormateada = fechaActual.toLocaleDateString("en-US", opciones); 
 
   fechaElemento.textContent = `Published ${fechaFormateada}`;
 }
 
-// Llamar a la función cuando la página se haya cargado
 window.addEventListener("DOMContentLoaded", mostrarFechaActual);
